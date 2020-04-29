0a98c5365041d4b08bd202797a86f194f3e69095
Zach Waite <zach@waiteperspectives.com>
2020-04-29 03:07:56 -0400

    connector_simul boilerplate

---
+++ connector_simul/__manifest__.py
@@ -0,0 +1,37 @@
+{
+    'name': 'Odoo - Simul Connector',
+    'summary': """Connect to Simul Document Versioning Service""",
+    'category': 'Operations/Documents',
+    'version': '13.0.0.1',
+
+    'author': 'Waite Perspectives, LLC - Zach Waite',
+
+    'depends': [
+        'attachment_indexation',
+        'connector',
+    ],
+
+    'data': [
+    ],
+
+    'demo': [],
+
+    'qweb': [],
+
+    'images': [],
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+    'uninstall_hook': None,
+
+    'application': False,
+    'auto_install': False,
+    'installable': True,
+    # 'external_dependencies': {'python': [], 'bin': []},
+    'support': 'zach@waiteperspectives.com',
+    'website': 'www.waiteperspectives.com',
+
+    # 'license': 'LGPL-3',
+    # 'price': 999.00,
+    # 'currency': 'USD',
+}
